5cc8f88c12a4f623a43eaa810cae7c933caf4629
AbdullayevValeh <v.abdullayev1996@gmail.com>
2021-06-11 09:49:40 +0400

    Price

---
+++ src/main/java/az/code/unisubribtion/models/Price.java
@@ -0,0 +1,20 @@
+package az.code.unisubribtion.models;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "prices", schema = "public")
+@Entity
+public class Price {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String value;
+    private String type;
+}
