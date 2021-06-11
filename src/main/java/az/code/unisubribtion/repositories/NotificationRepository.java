32f6f0c8a614d9107f71e1c600f245dc07ea0e73
ParvinYagubzada <parvinyy@code.edu.az>
2021-06-12 00:32:45 +0400

    Final

--- src/main/java/az/code/unisubribtion/repositories/NotificationRepository.java
+++ src/main/java/az/code/unisubribtion/repositories/NotificationRepository.java
@@ -7,6 +7,8 @@ import az.code.unisubribtion.models.Notification;
 import java.util.List;
 
 public interface NotificationRepository extends PagingAndSortingRepository<Notification, Long> {
+
     List<Notification> getAllByUserId(Long userId);
+
     Notification getNotificationByUserIdAndSubscriptionIdAndId(Long userId, Long subscriptionId, Long notificationId);
 }
--- src/main/java/az/code/unisubribtion/repositories/UserRepository.java
+++ src/main/java/az/code/unisubribtion/repositories/UserRepository.java
@@ -5,6 +5,7 @@ import az.code.unisubribtion.models.SubscriptionUser;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<SubscriptionUser, Long> {
+
     SubscriptionUser findUserByUsername(String username);
 
     SubscriptionUser findUserByEmail(String email);
