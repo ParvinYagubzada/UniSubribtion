75d4d6c823f99f64a83bdb526dd2027c85511401
AbdullayevValeh <v.abdullayev1996@gmail.com>
2021-06-11 17:58:25 +0400

    notification service

---
+++ src/main/java/az/code/unisubribtion/services/NotificationService.java
@@ -0,0 +1,11 @@
+package az.code.unisubribtion.services;
+
+import az.code.unisubribtion.models.Notification;
+
+import java.util.List;
+
+public interface NotificationService {
+    Notification createNotification(Long userId, Long subscriptionId, Notification notification);
+    List<Notification> getAllNotifications(Long userId);
+    Long deleteNotification(Long userId, Long subscriptionId, Long notificationId);
+}
