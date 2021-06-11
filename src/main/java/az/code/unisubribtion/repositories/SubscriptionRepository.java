e2441eb92b0bc15a2d2ce71d5176ff44b87b5c3f
AbdullayevValeh <v.abdullayev1996@gmail.com>
2021-06-11 12:51:43 +0400

    update

--- src/main/java/az/code/unisubribtion/repositories/SubscriptionRepository.java
+++ src/main/java/az/code/unisubribtion/repositories/SubscriptionRepository.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 public interface SubscriptionRepository extends PagingAndSortingRepository<Subscription, Long> {
     Page<Subscription> findSubscriptionsByUserId (Long userId, Pageable pageable);
-    Page<Subscription> findSubscriptionsByCategoryId (Long categoryId, Pageable pageable);
-    Page<Subscription> findSubscriptionsByGroupId (Long groupId, Pageable pageable);
+    Page<Subscription> findSubscriptionsByUserIdAndCategoryId (Long userId, Long categoryId, Pageable pageable);
+    Page<Subscription> findSubscriptionsByUserIdAndGroupId (Long userId, Long groupId, Pageable pageable);
+
 }
