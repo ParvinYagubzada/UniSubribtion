e970b682a422827ba39fc02ac422c0edbf8904a4
Parvin Yaqubzada <pyaqubzade@std.beu.edu.az>
2021-06-11 09:33:17 +0400

    Initial commit

---
+++ src/main/java/az/code/unisubribtion/UniSubribtionApplication.java
@@ -0,0 +1,13 @@
+package az.code.unisubribtion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniSubribtionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniSubribtionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/code/unisubribtion/UniSubribtionApplicationTests.java
@@ -0,0 +1,13 @@
+package az.code.unisubribtion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniSubribtionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
