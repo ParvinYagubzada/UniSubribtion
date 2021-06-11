d039b5ded2c3a72b9c6df71f5a07a6072a52c7f5
ParvinYagubzada <parvinyy@code.edu.az>
2021-06-12 02:18:41 +0400

    Noting in particular.

--- src/main/java/az/code/unisubribtion/services/NotificationService.java
+++ src/main/java/az/code/unisubribtion/services/NotificationService.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 public interface NotificationService {
     Notification createNotification(Long userId, Long subscriptionId, Notification notification);
+
     List<Notification> getAllNotifications(Long userId);
+
     Long deleteNotification(Long userId, Long subscriptionId, Long notificationId);
 }
